9f758ad13134dfefa0a92c0755ec5800609e6bb1
Neath3333 <daneath@saerosoft.com>
2025-12-19 23:53:35 +0700

    feat: rendering Trending , popular , and Upcoming movies in Discover page

--- app/discover/page.jsx
+++ app/discover/page.jsx
@@ -1,5 +1,32 @@
-// export default function Navbar(){
-//     return (
-//         nav
-//     )
-// }
+import React from "react";
+import { getTrendingMovies,getPopularMovies,getUpcomingMovies } from "../../lib/tmdb";
+
+export default async function DiscoverPage() {
+  const trending = await getTrendingMovies();
+  const popular = await getPopularMovies();
+  const upcoming = await getUpcomingMovies();   
+
+
+return(
+    <div>
+        <h1>Discover Movies</h1>
+        <h2>Trending</h2>
+        <ul>
+            {trending.results.map(movie=>{
+                <li key={movie.id}>{movie.title}</li>
+            })}
+        </ul>
+        <h2>Popular Movies</h2>
+        <ul>
+            {popular.results.map(movie=>{
+                <li key={movie.id}>{movie.title}</li>
+            })}
+        </ul>
+        <h2>Upcoming Movies</h2>
+        <ul>
+            {upcoming.results.map(movie=>{
+                <li key={movie.id}>{movie.title}</li>
+            })}
+        </ul>
+    </div>
+)};
