cfc715edcd2ef28f3a434984ea2b8f32d9f5ed93
Neath3333 <daneath@saerosoft.com>
2026-02-07 23:58:40 +0700

    feat(homepage): display movie data in homepage

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { getTrendingMovies } from "@/lib/tmdb";
+import MovieCard from "@/components/MovieCard";
 
 //import MovieCard from '../components/MovieCard';
 
@@ -10,6 +12,14 @@ export default function Home() {
 return (
     <div className='bg-blue-900'>
    {/* <MovieCard  /> */}
+    <h1 className="text-2xl font-bold mb-4">
+        Trending Movies Today
+      </h1>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4">
+        {trendingMovies.slice(0, 10).map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
     </div>
   );
 }
